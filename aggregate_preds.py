4ee88f6773729b1e2c37fabb4b7409a26f17c5fe
Viswonathan Manoranjan <viswonathan0606@gmail.com>
2022-02-25 10:44:37 +0400

    feat: Updated code
    Added code for all training and evaluation purposes.

--- aggregate_preds.py
+++ aggregate_preds.py
@@ -1,25 +1,31 @@
 from typing import final
 import pandas as pd
 
-path_to_fb = "./Results/fb_pred.csv"
+path_to_fb = "/mnt/Results/fb_pred.csv"
 
-path_to_textual = "./Results/alltextual_results.csv"
+path_to_textual = "/mnt/Results/alltextual_results.csv"
 
 df_fb = pd.read_csv(path_to_fb)
-df_textual = pd.read_csv(path_to_textual)
+df_textual = pd.read_csv(path_to_textual, header=None)
+
+# print(df_fb.index)
+# print(df_textual.index)
 
 df_fb = df_fb.reset_index(drop= True)
 
 df_fb = df_fb.drop(["Unnamed: 0","minute"], axis = 1)
 mean_df_fb = df_fb.groupby(['ID_y'], as_index=False).mean()
 mean_df_fb.columns = [0, 1, 2, 3, 4, 5]
+# print(mean_df_fb.head())
 
-print("textual")
-df_textual.drop([df_textual.columns[0],df_textual.columns[2]], axis = 1, inplace = True)
-print("After dropping:")
+# print("textual")
+# print(df_textual.head())
+df_textual.drop(df_textual.columns[1], axis = 1, inplace = True)
+# print("After dropping:")
+# print(df_textual.head())
 df_textual.columns = [0, 1, 2, 3, 4, 5]
-print(df_textual.head())
 mean_df_text = df_textual.groupby(df_textual.columns[0], as_index=False).mean()
+# print(mean_df_text.head(20))
 
 final_pred = mean_df_text.append(mean_df_fb)
 final_pred = final_pred.groupby(final_pred.columns[0], as_index=False).mean()
